8d811344f19ddee91b33d9784469780b3b08f4e3
Yamil J. Yunes <yunesyamil@gmail.com>
2024-06-26 19:57:06 -0300

    Java - Leccion02 - Enumeraciones

---
+++ Java/Leccion02/Enumeraciones/src/enumeraciones/Dias.java
@@ -0,0 +1,13 @@
+package enumeraciones;
+
+/*Cambiamos a class por enum, los elementos de la enum son publicos, 
+estaticos y finales (todos Constantes)*/
+public enum Dias {
+    LUNES,
+    MARTES, 
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO        
+}
