df3f9b6791a9c189ee69ae65fab04030b8ff6e92
Yamil J. Yunes <yunesyamil@gmail.com>
2024-07-01 18:51:44 -0300

    Python - Leccion04 - DB PostgreSQL

---
+++ Python/Tecnicatura3Py/Leccion04/DB/transacciones.py
@@ -0,0 +1,27 @@
+import psycopg2 as bd  # Esto es para poder conectarnos a PostgreSQL
+
+conexion = bd.connect(
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    # conexion.autocommit = False # esto directamente no deberia estar
+    cursor = conexion.cursor()
+    sentencia = 'INSERT INTO persona(nombre, apellido,email) VALUES (%s, %s, %s)'
+    valores = ('Maria', 'Esparza', 'mesparza@mail.com')
+    cursor.execute(sentencia, valores)
+    conexion.commit()  # Hacemos el commit manualmente
+    print('Termina la transacción')
+
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrio un error, se hizo un rollback: {e}')
+finally:
+    # cursor.close() el cierre del cursor lo esta haciendo el with
+    conexion.close()
+
+# Documentación de la conexion a la base de datos
+# https://www.psycopg.org/docs/usage.html
